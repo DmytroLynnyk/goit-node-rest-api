bd77951611d0bbfee78a2bd524cc9a7b05f2d1e5
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-04-14 11:53:57 +0300

    versionKey + contactsSchemas(email)

--- models/contacts.js
+++ models/contacts.js
@@ -1,22 +1,27 @@
 import { Schema, model } from "mongoose";
 import { handleMongooseError } from "../helpers/handleMongooseError.js";
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 contactSchema.post("save", handleMongooseError);
 
