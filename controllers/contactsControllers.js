b35a9f06f4a577c87dac297245deb80efb32b927
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-04-29 12:20:12 +0300

    favorite: all

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -7,7 +7,7 @@ export const getAllContacts = async (req, res, next) => {
     const { _id } = req.user;
     const { page = 1, limit = 20 } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner: _id, favorite: true }, "", {
+    const result = await Contact.find({ owner: _id }, "", {
       skip,
       limit: Number(limit),
     }).populate("owner", "_id email");
